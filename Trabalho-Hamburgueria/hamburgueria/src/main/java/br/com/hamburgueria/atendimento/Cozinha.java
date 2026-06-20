4545fcf92e2ed592e1921d8d5a69ce87103f21e5
Lucas-Mello851 <145824124+Lucas-Mello851@users.noreply.github.com>
2026-06-20 16:48:12 -0300

    Uptade Cozinha
    Alterando a cozinha para termos os novos tipos de preparo e ao mesmo tempo para que ela possa ter um catalogo de receitas

--- Trabalho-Hamburgueria/hamburgueria/src/main/java/br/com/hamburgueria/atendimento/Cozinha.java
+++ Trabalho-Hamburgueria/hamburgueria/src/main/java/br/com/hamburgueria/atendimento/Cozinha.java
@@ -1,9 +1,14 @@
 package br.com.hamburgueria.atendimento;
 
+import br.com.hamburgueria.dominio.Pedido;
+
 public class Cozinha {
 
     private CentralAtendimento mediator;
     private String pedidoAtual;
+    private Pedido pedidoEmPreparo;
+    private final CatalogoReceitas catalogoReceitas = new CatalogoReceitas();
+    private java.util.List<String> ultimasEtapasPreparo;
 
     public void setMediator(CentralAtendimento mediator) {
         this.mediator = mediator;
@@ -14,11 +19,34 @@ public class Cozinha {
         mediator.notificar(this, "pedido_recebido");
     }
 
+    public void receberPedido(Pedido pedido) {
+        this.pedidoEmPreparo = pedido;
+        this.pedidoAtual = pedido.getLanche().getDescricao();
+        mediator.notificar(this, "pedido_recebido");
+    }
+
     public void prepararPedido() {
+        if (pedidoEmPreparo != null) {
+            this.ultimasEtapasPreparo = catalogoReceitas.etapasDe(pedidoAtual);
+            pedidoEmPreparo.marcarPronto();
+        }
         mediator.notificar(this, "pedido_pronto");
+    }
+
+    public java.util.List<String> getUltimasEtapasPreparo() {
+        return ultimasEtapasPreparo;
+    }
+
+    public CatalogoReceitas getCatalogoReceitas() {
+        return catalogoReceitas;
     }
 
     public String getPedidoAtual() {
         return pedidoAtual;
     }
+
+    public Pedido getPedidoEmPreparo() {
+        return pedidoEmPreparo;
+    }
 }
+
