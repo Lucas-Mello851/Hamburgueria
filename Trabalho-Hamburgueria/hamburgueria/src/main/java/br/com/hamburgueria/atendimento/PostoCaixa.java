2d30ee91c901ccfe29909d341aeff883199afb01
Lucas-Mello851 <145824124+Lucas-Mello851@users.noreply.github.com>
2026-06-20 18:15:14 -0300

    Update PostoCaixa
    Atualizando Posto Caixa, para ter recibo, forma de pagamento, pedido montado e montador pedido, o método adicionado é o recibo de pagamento os outros são para comunicação

--- Trabalho-Hamburgueria/hamburgueria/src/main/java/br/com/hamburgueria/atendimento/PostoCaixa.java
+++ Trabalho-Hamburgueria/hamburgueria/src/main/java/br/com/hamburgueria/atendimento/PostoCaixa.java
@@ -1,9 +1,15 @@
 package br.com.hamburgueria.atendimento;
 
+import br.com.hamburgueria.pagamento.ReciboPagamento;
+import br.com.hamburgueria.pagamento.FormaPagamento;
+import br.com.hamburgueria.pedidos.PedidoMontado;
+import br.com.hamburgueria.pedidos.MontadorPedido;
+
 public class PostoCaixa {
 
     private CentralAtendimento mediator;
     private boolean entregaConfirmada = false;
+    private ReciboPagamento ultimoRecibo;
 
     public void setMediator(CentralAtendimento mediator) {
         this.mediator = mediator;
@@ -11,6 +17,19 @@ public class PostoCaixa {
 
     public void registrarPedido(String pedido) {
         mediator.notificar(this, "pedido_registrado:" + pedido);
+    }
+
+    public ReciboPagamento emitirRecibo(String tipoLanche, FormaPagamento forma, double valor) {
+        PedidoMontado montado = new MontadorPedido()
+                .setTipoLanche(tipoLanche)
+                .setFormaPagamento(forma.getNome())
+                .build();
+        this.ultimoRecibo = new ReciboPagamento(montado, forma, valor);
+        return ultimoRecibo;
+    }
+
+    public ReciboPagamento getUltimoRecibo() {
+        return ultimoRecibo;
     }
 
     public void confirmarEntrega() {
@@ -21,3 +40,4 @@ public class PostoCaixa {
         return entregaConfirmada;
     }
 }
+
